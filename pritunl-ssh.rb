f415e9b08f6c0ce0b5a3de2d49680b2671206cad
Zachary Huff <zach.huff.386@gmail.com>
2020-06-20 21:06:14 -0400

    Update version 1.0.1671.4

--- pritunl-ssh.rb
+++ pritunl-ssh.rb
@@ -1,9 +1,9 @@
 class PritunlSsh < Formula
   desc "Pritunl Zero SSH Client"
   homepage "https://zero.pritunl.com"
-  version "1.0.1623.92"
+  version "1.0.1671.4"
   url "https://github.com/pritunl/pritunl-zero-client/archive/#{version}.tar.gz"
-  sha256 "300326aad11b77b6e4dab9f5fbc1c7d88a85199549d1c5d4c0ed95bea9581c2c"
+  sha256 "0de32b062dfc63affdd0dc02c61768490d8e83382006f202b6af3cba95c2c5b9"
 
   def install
     binary_name = "pritunl-ssh"
